2a36aea47241f0937ad93fb311b645b9b39e6df7
Jure Slak <jure.slak@gmail.com>
2019-11-27 00:09:09 +0100

    Boljši opis.

--- GeneratorNalog.m
+++ GeneratorNalog.m
@@ -1,202 +1,207 @@
 classdef GeneratorNalog < handle
-    % Razred za generiranje nalog za Moodle kvize.
-    %
-    % Dokumentacija za Moodle:
-    %    https://docs.moodle.org/36/en/Numerical_question_type
+  % Razred za generiranje nalog za Moodle kvize.
+  %
+  % Dokumentacija za Moodle:
+  %    https://docs.moodle.org/36/en/Numerical_question_type
 
-    properties
-        ime_kviza;  % Človeško ime kviza, npr. '1. kviz 2018/19'
-        ime_naloge;   % Človeško ime naloge, npr. 'Naloga 1'.
-        ime_datoteke;  % Ime npr. 'naloga1.xml'
-        st_nalog;  % Koliko nalog naj se zgenerira.
-        st_vprasanj;  % Koliko okenc za odgovor naj ima ta naloga.
-        relativna_toleranca;  % Relativna toleranca napake študenta.
-        generator_parametrov;  % Funkcija, ki generira parametre.
-        koda_za_parametre;  % Koda, ki naj jo študent izvede na začetku.
-        resitev_naloge;  % Tvoja funkcija, ki reši nalogo.
-        uvod;  % Začetek naloge, ki vsebuje kodo za parametre in kaksne definicije za celo nalogo.'
-        vprasanja = cell(1);  % Seznam vprašanj.
+  properties
+    ime_kviza;  % Človeško ime kviza, npr. '1. kviz 2018/19'
+    ime_naloge;   % Človeško ime naloge, npr. 'Naloga 1'.
+    ime_datoteke;  % Ime npr. 'naloga1.xml'
+    st_nalog;  % Koliko nalog naj se zgenerira.
+    st_vprasanj;  % Koliko okenc za odgovor naj ima ta naloga.
+    relativna_toleranca;  % Relativna toleranca napake študenta.
+    generator_parametrov;  % Funkcija, ki generira parametre.
+    koda_za_parametre;  % Koda, ki naj jo študent izvede na začetku.
+    resitev_naloge;  % Tvoja funkcija, ki reši nalogo.
+    uvod;  % Začetek naloge, ki vsebuje kodo za parametre in kaksne definicije za celo nalogo.'
+    vprasanja = cell(1);  % Seznam vprašanj.
+  end
+
+  properties (SetAccess = private)
+    rezultati;
+    parametri;
+  end
+
+  methods
+    function obj = GeneratorNalog(ime_kviza, ime_naloge, ime_datoteke)
+      % Naredi generator nalog.
+      %
+      %  ime_naloge: Človeško ime naloge, npr. 'Naloga 1'.
+      %  ime_datoteke: Ime datoteke brez '.xml' končnice, npr.
+      %      'naloga1.' Privzeto enako kot ime_naloge.
+      rng(0);
+      assert(nargin >= 2, 'Podaj ime kviza in naloge!');
+      if nargin < 3, ime_datoteke = [ime_naloge '.xml']; end
+
+      obj.ime_kviza = ime_kviza;
+      obj.ime_naloge = ime_naloge;
+      obj.ime_datoteke = ime_datoteke;
     end
 
-    properties (SetAccess = private)
-        rezultati;
-        parametri;
+    function obj = generiraj(obj)
+      obj.check();
+
+      obj.parametri = cell(obj.st_nalog, 1);
+      obj.rezultati = zeros(obj.st_vprasanj, obj.st_nalog);
+
+      for i = 1:obj.st_nalog
+        obj.parametri{i} = obj.nakljucna_koda_za_parametre();
+        result = obj.resitev_naloge(obj.parametri{i});
+        assert(isfloat(result), ['Rezultat naloge s parametri '...
+          '"%s" ni vektor decimalnih števil.'], obj.parametri{i});
+        n = numel(result);
+        assert(n == obj.st_vprasanj, ['Rezultat bi moral imeti '...
+          '%d elementov, dobil sem jih %d'], obj.st_vprasanj, n);
+        obj.rezultati(:, i) = result(:);
+      end
+
+      if any(any(abs(obj.rezultati) < 1e-6))
+        warning('Nekateri rezultati so zelo blizu 0!');
+      end
+
+      if ~all(all(isfinite(obj.rezultati)))
+        warning('Nekateri rezultati niso končni!');
+      end
+
+      obj.shrani()
+      fprintf('Datoteka "%s" uspešno ustvarjena.\n', obj.ime_datoteke);
+    end
+  end
+
+  methods(Access=private)
+
+    function t = nakljucna_koda_za_parametre(obj)
+      c = count(obj.koda_za_parametre, '%%');
+      k = count(obj.koda_za_parametre, '%') - 2*c;
+      assert(k > 0, 'Začetna koda mora vsebovati vsaj eno dinamično polje.');
+      params = zeros(k, 0);
+      for i = 1:k, params(i) = obj.generator_parametrov(); end
+      t = sprintf(obj.koda_za_parametre, params);
     end
 
-    methods
-        function obj = GeneratorNalog(ime_kviza, ime_naloge, ime_datoteke)
-            % Naredi generator nalog.
-            %
-            %  ime_naloge: Človeško ime naloge, npr. 'Naloga 1'.
-            %  ime_datoteke: Ime datoteke brez '.xml' končnice, npr.
-            %      'naloga1.' Privzeto enako kot ime_naloge.
-            rng(0);
-            assert(nargin >= 2, 'Podaj ime kviza in naloge!');
-            if nargin < 3, ime_datoteke = [ime_naloge '.xml']; end
+    function check(obj)
+      obj.check_string('Ime naloge', obj.ime_naloge);
+      obj.check_string('Ime kviza', obj.ime_kviza);
+      obj.ime_naloge = replace(obj.ime_naloge, '/', '//');  % v Moodlu so kategorije ločene z /, tako da če želimo / v imenu, ga moramo ponoviti dvakrat
+      obj.ime_kviza = replace(obj.ime_kviza, '/', '//');
+      obj.check_string('Ime datoteke', obj.ime_datoteke);
+      obj.check_string('Koda za parametre', obj.koda_za_parametre);
+      obj.check_string('Uvod naloge', obj.uvod);
 
-            obj.ime_kviza = ime_kviza;
-            obj.ime_naloge = ime_naloge;
-            obj.ime_datoteke = ime_datoteke;
-        end
+      obj.st_nalog = obj.check_int('Število nalog', obj.st_nalog);
+      obj.st_vprasanj = obj.check_int('Število vprašanj', obj.st_vprasanj);
 
-        function obj = generiraj(obj)
-            obj.check();
+      assert(0 < obj.relativna_toleranca, ...
+        'Relativna toleranca mora biti pozitivna, dobil sem %d.', obj.relativna_toleranca);
+      if obj.relativna_toleranca > 1e-2
+        warning(['Relativna toleranca %g je precej visoka - tudi kakšen napačen rezultat bi lahko bil sprejet.'], obj.relativna_toleranca);
+      end
+      assert(0 < obj.st_nalog, 'Število decimalk mora biti pozitivno, dobil sem %d.');
+      if obj.st_nalog > 200, warning('Število nalog %d je zelo veliko!', obj.st_nalog); end
+      assert(0 < obj.st_vprasanj, 'Število vprašanj mora biti pozitivno, dobil sem %d.', obj.st_vprasanj);
+      if obj.st_vprasanj > 10, warning('Število vprašanj %d je zelo veliko!', obj.st_vprasanj); end
 
-            obj.parametri = cell(obj.st_nalog, 1);
-            obj.rezultati = zeros(obj.st_vprasanj, obj.st_nalog);
 
-            for i = 1:obj.st_nalog
-                obj.parametri{i} = obj.nakljucna_koda_za_parametre();
-                result = obj.resitev_naloge(obj.parametri{i});
-                assert(isfloat(result), ['Rezultat naloge s parametri '...
-                    '"%s" ni vektor decimalnih števil.'], obj.parametri{i});
-                n = numel(result);
-                assert(n == obj.st_vprasanj, ['Rezultat bi moral imeti '...
-                    '%d elementov, dobil sem jih %d'], obj.st_vprasanj, n);
-                obj.rezultati(:, i) = result(:);
-            end
+      assert(iscell(obj.vprasanja), 'Vprašanja morajo biti cell dolžine %d.', obj.st_vprasanj);
+      assert(numel(obj.vprasanja) == obj.st_vprasanj, 'Vprašanja morajo biti cell dolžine %d.', obj.st_vprasanj);
+      obj.vprasanja = obj.vprasanja(:);
 
-            if any(any(abs(obj.rezultati))) < 1e-6
-                warning('Nekateri rezultati so zelo blizu 0!');
-            end
+      for i = 1:obj.st_vprasanj
+        obj.check_string(sprintf('Vprašanje %d', i), obj.vprasanja{i});
+        obj.vprasanja{i} = ['<br><br>' obj.vprasanja{i} ' '];
+      end
 
-            obj.shrani()
-            fprintf('Datoteka "%s" uspešno ustvarjena.\n', obj.ime_datoteke);
-        end
+      assert(isa(obj.generator_parametrov, 'function_handle'),...
+        'Generator parametrov mora biti funkcija, ki generira naključne parametre');
+      assert(isa(obj.resitev_naloge, 'function_handle'),...
+        'Resitev naloge mora biti funkcija, ki sprejme zacetno kodo in ',...
+        'vrne vektor odgovor (za vsako vprasanje enega).');
+
     end
 
-    methods(Access=private)
+    function shrani(obj)
+      fid = fopen(obj.ime_datoteke, 'w');
+      assert(fid ~= -1, 'Ne morem odpreti datoteke ''%s''.', obj.ime_datoteke);
 
-        function t = nakljucna_koda_za_parametre(obj)
-            c = count(obj.koda_za_parametre, '%%');
-            k = count(obj.koda_za_parametre, '%') - 2*c;
-            assert(k > 0, 'Začetna koda mora vsebovati vsaj eno dinamično polje.');
-            params = zeros(k, 0);
-            for i = 1:k, params(i) = obj.generator_parametrov(); end
-            t = sprintf(obj.koda_za_parametre, params);
+      fprintf(fid, '<?xml version="1.0"?>\n');
+      fprintf(fid, '<quiz>\n');
+      % question category
+      fprintf(fid, '  <question type="category">\n');
+      fprintf(fid, '    <category><text><![CDATA[$course$/%s/%s]]></text></category>\n', obj.ime_kviza, obj.ime_naloge);
+      fprintf(fid, '  </question>\n\n');
+      % description type question with question code inside
+      fprintf(fid, '<!-- question: 1  -->\n');
+      fprintf(fid, '  <question type="description">\n');
+      fprintf(fid, '    <name><text><![CDATA[Uvod]]></text></name>\n');
+      fprintf(fid, '    <questiontext format="html">\n');
+      fprintf(fid, '      <text><![CDATA[uvod]]></text>\n');
+      fprintf(fid, '    </questiontext>\n');
+      fprintf(fid, '    <image></image>\n');
+      fprintf(fid, '    <generalfeedback>\n');
+      fprintf(fid, '      <text></text>\n');
+      fprintf(fid, '    </generalfeedback>\n');
+      fprintf(fid, '    <defaultgrade>0</defaultgrade>\n');
+      fprintf(fid, '    <penalty>0</penalty>\n');
+      fprintf(fid, '    <hidden>0</hidden>\n');
+      fprintf(fid, '    <shuffleanswers>0</shuffleanswers>\n');
+      fprintf(fid, '</question>\n\n');
+      % insert questions into this category
+      fprintf(fid, '  <question type="category">\n');
+      fprintf(fid, '    <category><text><![CDATA[$course$/%s/%s]]></text></category>\n', obj.ime_kviza, obj.ime_naloge);
+      fprintf(fid, '  </question>\n\n');
+      % questions
+      for i = 1:obj.st_nalog
+        % question header
+        fprintf(fid, '\n  <!-- question %d -->\n', i);
+        fprintf(fid, '  <question type="cloze">\n');
+        fprintf(fid, '    <name><text><![CDATA[Vprašanje %02d]]></text></name>\n', i);
+        fprintf(fid, '    <questiontext>\n');
+        fprintf(fid, '      <text><![CDATA[\n');
+
+        % Uvod v nalogo
+        fprintf(fid, 'Izvedite naslednje ukaze: <pre>%s</pre>\n', obj.parametri{i});
+        fprintf(fid, '%s\n\n', obj.uvod);
+
+        for j = 1:obj.st_vprasanj
+          % Tekst vprašanja
+          fprintf(fid, '%s\n', obj.vprasanja{j});
+          % Okence za odgovor
+          fprintf(fid, '{1:NUMERICAL:=%.16f:%.16f}\n\n', obj.rezultati(j, i), obj.relativna_toleranca*obj.rezultati(j, i));
         end
 
-        function check(obj)
-            obj.check_string('Ime naloge', obj.ime_naloge);
-            obj.check_string('Ime kviza', obj.ime_kviza);
-            obj.ime_naloge = replace(obj.ime_naloge, '/', '//');  % v Moodlu so kategorije ločene z /, tako da če želimo / v imenu, ga moramo ponoviti dvakrat
-            obj.ime_kviza = replace(obj.ime_kviza, '/', '//');
-            obj.check_string('Ime datoteke', obj.ime_datoteke);
-            obj.check_string('Koda za parametre', obj.koda_za_parametre);
-            obj.check_string('Uvod naloge', obj.uvod);
-
-            obj.st_nalog = obj.check_int('Število nalog', obj.st_nalog);
-            obj.st_vprasanj = obj.check_int('Število vprašanj', obj.st_vprasanj);
-
-            assert(0 < obj.relativna_toleranca, ...
-                'Relativna toleranca mora biti pozitivna, dobil sem %d.', obj.relativna_toleranca);
-            if obj.relativna_toleranca > 1e-2, warning(['Relativna toleranca %g je precej visoka '...
-                    '- tudi kakšen napačen rezultat bi lahko bil sprejet.'], obj.relativna_toleranca); end
-                assert(0 < obj.st_nalog, 'Število decimalk mora biti pozitivno, dobil sem %d.');
-                if obj.st_nalog > 200, warning('Število nalog %d je zelo veliko!', obj.st_nalog); end
-                assert(0 < obj.st_vprasanj, 'Število vprašanj mora biti pozitivno, dobil sem %d.', obj.st_vprasanj);
-                if obj.st_vprasanj > 10, warning('Število vprašanj %d je zelo veliko!', obj.st_vprasanj); end
+        % question footer
+        fprintf(fid, '      ]]></text>\n');
+        fprintf(fid, '    </questiontext>\n');
+        fprintf(fid, '    <shuffleanswers>0</shuffleanswers>\n');
+        fprintf(fid, '  </question>\n');
+      end
+      %
+      fprintf(fid, '</quiz>\n');
 
 
-                assert(iscell(obj.vprasanja), 'Vprašanja morajo biti cell dolžine %d.', obj.st_vprasanj);
-                assert(numel(obj.vprasanja) == obj.st_vprasanj, 'Vprašanja morajo biti cell dolžine %d.', obj.st_vprasanj);
-                obj.vprasanja = obj.vprasanja(:);
-
-                for i = 1:obj.st_vprasanj
-                    obj.check_string(sprintf('Vprašanje %d', i), obj.vprasanja{i});
-                    obj.vprasanja{i} = ['<br><br>' obj.vprasanja{i} ' '];
-                end
-
-                assert(isa(obj.generator_parametrov, 'function_handle'),...
-                    'Generator parametrov mora biti funkcija, ki generira naključne parametre');
-                assert(isa(obj.resitev_naloge, 'function_handle'),...
-                    'Resitev naloge mora biti funkcija, ki sprejme zacetno kodo in ',...
-                    'vrne vektor odgovor (za vsako vprasanje enega).');
-
-            end
-
-            function shrani(obj)
-                fid = fopen(obj.ime_datoteke, 'w');
-                assert(fid ~= -1, 'Ne morem odpreti datoteke ''%s''.', obj.ime_datoteke);
-
-                fprintf(fid, '<?xml version="1.0"?>\n');
-                fprintf(fid, '<quiz>\n');
-                % question category
-                fprintf(fid, '  <question type="category">\n');
-                fprintf(fid, '    <category><text><![CDATA[$course$/%s/%s]]></text></category>\n', obj.ime_kviza, obj.ime_naloge);
-                fprintf(fid, '  </question>\n\n');
-                % description type question with question code inside
-                fprintf(fid, '<!-- question: 1  -->\n');
-                fprintf(fid, '  <question type="description">\n');
-                fprintf(fid, '    <name><text><![CDATA[Uvod]]></text></name>\n');
-                fprintf(fid, '    <questiontext format="html">\n');
-                fprintf(fid, '      <text><![CDATA[uvod]]></text>\n');
-                fprintf(fid, '    </questiontext>\n');
-                fprintf(fid, '    <image></image>\n');
-                fprintf(fid, '    <generalfeedback>\n');
-                fprintf(fid, '      <text></text>\n');
-                fprintf(fid, '    </generalfeedback>\n');
-                fprintf(fid, '    <defaultgrade>0</defaultgrade>\n');
-                fprintf(fid, '    <penalty>0</penalty>\n');
-                fprintf(fid, '    <hidden>0</hidden>\n');
-                fprintf(fid, '    <shuffleanswers>0</shuffleanswers>\n');
-                fprintf(fid, '</question>\n\n');
-                % insert questions into this category
-                fprintf(fid, '  <question type="category">\n');
-                fprintf(fid, '    <category><text><![CDATA[$course$/%s/%s]]></text></category>\n', obj.ime_kviza, obj.ime_naloge);
-                fprintf(fid, '  </question>\n\n');
-                % questions
-                for i = 1:obj.st_nalog
-                    % question header
-                    fprintf(fid, '\n  <!-- question %d -->\n', i);
-                    fprintf(fid, '  <question type="cloze">\n');
-                    fprintf(fid, '    <name><text><![CDATA[Vprašanje %02d]]></text></name>\n', i);
-                    fprintf(fid, '    <questiontext>\n');
-                    fprintf(fid, '      <text><![CDATA[\n');
-
-                    % Uvod v nalogo
-                    fprintf(fid, 'Izvedite naslednje ukaze: <pre>%s</pre>\n', obj.parametri{i});
-                    fprintf(fid, '%s\n\n', obj.uvod);
-
-                    for j = 1:obj.st_vprasanj
-                        % Tekst vprašanja
-                        fprintf(fid, '%s\n', obj.vprasanja{j});
-                        % Okence za odgovor
-                        fprintf(fid, '{1:NUMERICAL:=%.16f:%.16f}\n\n', obj.rezultati(j, i), obj.relativna_toleranca*obj.rezultati(j, i));
-                    end
-
-                    % question footer
-                    fprintf(fid, '      ]]></text>\n');
-                    fprintf(fid, '    </questiontext>\n');
-                    fprintf(fid, '    <shuffleanswers>0</shuffleanswers>\n');
-                    fprintf(fid, '  </question>\n');
-                end
-                %
-                fprintf(fid, '</quiz>\n');
-
-
-                status = fclose(fid);
-                assert(status ~= -1, 'Ne morem zapreti datoteke ''%s''.', obj.ime_datoteke);
-            end
-        end
-
-        methods(Static, Access=private)
-            function value = check_int(param, value)
-                GeneratorNalog.check_nonempty(param, value)
-                assert(isreal(value), '%s mora biti število, dobil sem tip ''%s''.', param, class(value));
-                n = numel(value);
-                assert(n == 1, '%s mora biti število, dobil sem %d vrednosti.', param, n);
-                value = value(:);
-                assert(mod(value, 1) == 0, '%s mora biti celo število, dobil sem %f.', param, value);
-            end
-
-            function check_string(param, value)
-                GeneratorNalog.check_nonempty(param, value)
-                assert(ischar(value), '%d mora biti niz znakov, dobil sem tip ''%s''.', param, class(value));
-            end
-
-            function check_nonempty(param, value)
-                assert(~isempty(value), 'Podaj %s!', lower(param));
-            end
-        end
+      status = fclose(fid);
+      assert(status ~= -1, 'Ne morem zapreti datoteke ''%s''.', obj.ime_datoteke);
     end
+  end
+
+  methods(Static, Access=private)
+    function value = check_int(param, value)
+      GeneratorNalog.check_nonempty(param, value)
+      assert(isreal(value), '%s mora biti število, dobil sem tip ''%s''.', param, class(value));
+      n = numel(value);
+      assert(n == 1, '%s mora biti število, dobil sem %d vrednosti.', param, n);
+      value = value(:);
+      assert(mod(value, 1) == 0, '%s mora biti celo število, dobil sem %f.', param, value);
+    end
+
+    function check_string(param, value)
+      GeneratorNalog.check_nonempty(param, value)
+      assert(ischar(value), '%d mora biti niz znakov, dobil sem tip ''%s''.', param, class(value));
+    end
+
+    function check_nonempty(param, value)
+      assert(~isempty(value), 'Podaj %s!', lower(param));
+    end
+  end
+end
--- README.md
+++ README.md
@@ -4,19 +4,37 @@ Ta dokument opisuje kako uporabljati skripto za generiranje
 vprašanj za ustvarjanje kvizov na spletni učilnici Moodle
 za preverjanje znanja pri numeričnih predmetih na FMF.
 
-## Uporaba razreda za geneiranje XML nalog
+## Uporaba razreda za generiranje XML nalog
 
-1. Skopiraj skrpto `test_generator.m` in jo preimenuj npr. v `naloga1.m`. Imeti mora dostop do
+Za uporabo kode za generiranje kvizov potrebujete Matlab (gotovo deluje na verziji 2017a,
+vendar najverjetneje tudi na nekaj starejših in novejših).
+
+1. Skopiraj skripto `test_generator.m` in jo preimenuj npr. v `naloga1.m`. Imeti mora dostop do
    razreda `GeneratorNalog` (lahko tudi datoteko `GeneratorNalog.m` skopiraš v isto mapo ali pa
    uporabiš ukaz `addpath`.
 
 1. Nastavi metapodatke: ime kviza, število vprašanj, število študentov, toleranco, izhodno datoteko, itd... Nato
    nastavi naključno generiranje podatkov, napiši besedilo nalog in rešitev. Vse rešitve se
-poženejona podatkih, ki jih bodo dobili študenti, tako da lahko preveriš delovanje, se pa vsaka
+poženejo na podatkih, ki jih bodo dobili študenti, tako da lahko preveriš delovanje, se pa vsaka
 rešitev (če ne uporabljaš globalnih spremenljivk) požene neodvisno od ostalih.
 
 1. Poženi datoteko `naloga1.m`, ki zgenerira datoteko `naloga1.xml` (oz. kakor je bilo nastavljeno),
    ki jo potrebujemo za spletno učilnico.
+
+1. Ponovi za vse naloge, ki pa si naj imajo enako ime kviza.
+
+## Novosti
+
+* ne potrebujete Mathematice, samo vključite eno datoteko v Matlabu
+* ena datoteka za definicijo naloge, ena datoteka s podatki za Moodle
+* kategorije se avtomatsko uvozijo, tako da jih ni treba ustvarjati ročno
+* boljša podpora za generiranje naključnih podatkov, ki ne vključujejo `rng(seed)` funkcije
+* manj možnosti za napake, saj se rešitev vsake generirane naloge požene znotraj funkcije neodvisno
+  od ostalih (ne morejo te motiti druge spremenljivke, ali spremenljivke iz prejšnje iteracije)
+  in z izvedeno točno kodo, ki jo bodo dobili študenti, kar zagotovi, da dejansko rešuješ problem z
+  enakimi podatki (in se ne zmotiš npr. pri izpisovanju zaradi premalo decimalk).
+* več preverjanja, da je število odgovorov enako številu besedil podnalog,
+da so odgovori končna decimalna števila ne preblizu 0, ...
 
 ## Ustvarjanje novega kviza
 
@@ -27,23 +45,64 @@ XML", ne potrebujete pa nobenih drugih nastavitev, saj je kljukica "pridobi poda
 privzeto označena in poskrbi za vse ostalo. Kliknete "Uvozi" in vprašanja se uvozijo. Enako ponovite
 za vse ostale 4 datoteke.
 
-Sedaj lahko naredite nov kviz preko "Vključi urejanje / dodaj dejavnost ali vir / kviz"
+Sedaj lahko naredite nov kviz preko "Vključi urejanje / dodaj dejavnost ali vir / kviz".
+Tu je potrebno nastaviti
 
-TODO: https://ucilnica1718.fmf.uni-lj.si/pluginfile.php/30497/mod_resource/content/0/Navodila%20za%20pripravo%20kvizov.pdf
+* časovna uskladitev: 60 min (omogoči), odprti poskusi naj se zaključijo in oddajo avtomatično (omogoči)
+* postavitev: nova stran: vsakih 5 vprašanj (da bodo vse naloge na eni strani)
+* obnašanje vprašanja: kako se vprašanja vedejo: prilagojen način (brez odbitkov)
+* možnosti pregleda: pravilni odgovor (onemogoči)
+* dodatne omejitve pri poskusih: zahtevaj geslo: potrebno je nastaviti geslo, ki ga bodo študenti izvedeli v predavalnici
+
+Nato se gre na "Shrani in prikaži" in nato "Uredi kviz" (da dodamo še vprašanja).
+
+Preko menija "Dodaj / Dodaj naključno vprašanje / kategorijo z ustrezno nalogo / Dodaj izbrano vprašanje" dodamo
+vsako nalogo. Nato se popravi točkovanje in se vsaki nalogi da 2.5 točke (morda je treba vnesti
+decimalko s piko ali vejico, odvisno od nastavitev jezika). Nato kliknemo "Shrani". Stran se ne zapre, zato moramo
+potem ročno iti nazaj na kviz, da ga lahko testiramo.
 
 ## Napake
 
-Ko se vprašanja uvozijo jih dobite prikazana na zaslonu. Prvo vprašnaje ima vedno besedilo
+Ko se vprašanja uvozijo iz XML datotek jih dobite prikazana na zaslonu. Prvo vprašanje ima vedno besedilo
 "uvod", ostala pa morajo imeti pravilno prikazan LaTeX (če ste ga uporabili) z nekaj polji oblike `{#1}` za rezultate.
 Če po uvozu pri besedilu nalog opazite kakšno napako, morate uvožena vprašanja zbrisati, ponovno
-zgenerirati pravilno XML datoteko in jo ponovno uzoviti. Vprašanja najlažje izbrišete tako, da
+zgenerirati pravilno XML datoteko in jo ponovno uvoziti. Vprašanja najlažje izbrišete tako, da
 izberete "Naprej", jih označite in spodaj kliknete "Izbriši".
 
 Če ste se zmotili v imenu kategorije morate najprej izbrisati vsa vprašanja v njej,
 nato na izbrisati še kategorijo. Kategorijo lahko izbrišete preko menija "Zbirka vprašanj / Kategorije" s
 klikom na `x` zraven kategorije.
 
+Če so nastavitve kviza napačne, jih lahko uredite pred kvizom. Med kvizom večino nastavitev ni možno urejati, če pa
+se ureja nastavitve (npr. časovna omejitev, geslo) morajo študenti osvežiti stran ali pa zapustiti in se
+vrniti v kviz, da registrira spremembo.
 
-## Zgodovina:
+## Zgodovina
 
-TODO
+Besedilo zgoraj je povzeto po dokumentu, ki ga je Bor Plestenjak sestavil za Seminar NA dne 11. 11. 2015
+in je na voljo na USMAOŠ spletni učilnici.
+
+Sam generator kvizov, ki je bil originalno napisan v kombinaciji Mathematice in Matlaba izvira iz FE, prek
+Emila Žagarja, Andreja Muhiča, Bora Plestenjaka in Jureta Slaka, do trenutne kompaktnejše Matlab verzije.
+
+
+## Naprednejša uporaba
+
+Parametri razreda `GeneratorNalog` so:
+
+* parametri konstruktorja: 2 niza, ime kviza in ime naloge. V Moodlu bo ob uvozu ustvarjena
+  kategorija z imenom kviza in podkategorija z imenom naloge.
+* `st_nalog`: število zgenenriranih naključnih nalog, mora biti večje kot število študentov,
+  sicer se bodo naloge ponavljale.
+* `st_vprasanj`: koliko vprašanj (okenc za rezultate) bo imela ta naloga
+* `relativna_toleranca`: odgovor študenta bo veljal za pravilnega, če je njegova relativna napaka
+  pod to toleranco
+* `ime_datoteke`: ime zgenerirane datoteke (priporočljiva je končnica `.xml`.
+* `generator_parametrov`: TODO
+* `koda_za_parametre`: TODO
+* `uvod`: besedilo z uvodom za vsa vprašanja
+* `vprasanja`: cell array dolžine kolikor je vprašanj z opisom pozameznega vprašanja
+* `resitev_naloge`: function handle na rešitev naloge, ki kot parameter sprejme en niz z začetnimi
+  parametri, od kjer s pomočjo `eval` lahko dobi enake začetne podatke, kot jih bo videl študent.
+
+
